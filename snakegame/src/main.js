d7a20daff9798a0c2e2f24795339634c6ac4fa77
Víctor <142942997+viictor0906@users.noreply.github.com>
2025-04-17 17:46:49 -0300

    Update game code
    I'm workout in this project in recent weeks but i don't update the GitHub repository. So, i am update now with all features added.

--- snakegame/src/main.js
+++ snakegame/src/main.js
@@ -1,99 +1,134 @@
 import kaboom from "kaboom"
 
-const k = kaboom({
-    //Screen config
-    width:800,
-    height:600,
-    scale: 1,
-    stretch: true,
-    letterbox: true
-})
+kaboom();
 
-k.loadSprite("player", "sprites/bean.png")
+//Setting screen config and defining "k" as a 'get' of kaboom lib.
+    const k = kaboom({
+        width:800,
+        height:600,
+        scale:1,
+        stretch:false,
+        letterbox: true
+    });
+//End.
 
-const player = add([
-	k.pos(center()),
-	k.sprite("player"),{
-            speed: 200,
-            currentDir: "up"   
-        }
-])
+//Loading sprites for game.
+    k.loadSprite("player", "sprites/bean.png");
+    k.loadSprite("apple", "sprites/apple.png");
+//End.
 
-// Grid settings
-const GRID_SIZE = 32//Size of each grid cell.
-const COLS = 32//Number of columns.
-const ROWS = 32//Number of rows.
+//Defining game scene and start real game programming.
+    k.scene("game", () => {
+        let player = add([
+            k.sprite("player"),
+            k.pos(center()),
+            k.area(),
+            k.body(),
+            {
+                speed:200,
+                currentDir:"up"
+            },
+            "snake-head"
+        ]);
+//End.
 
-//Calc for centralized grid
-const gridWidth = COLS * GRID_SIZE
-const gridHeight = ROWS * GRID_SIZE
-const startX = (k.width() - gridWidth)/2//Centralized horizontal.
-const startY = (k.height() - gridHeight)/2//Centralized vertical.
+    let snakeBody = [];
 
-// Function to draw the grid
-function drawGrid() {
+//Function will be spawn random apples in the map.
+    function spawnApple(){
+        k.add([
+            k.sprite("apple"),
+            k.pos(rand(100,700), rand(100,500)),//Defining random position for each spawned.
+            k.area(),//Defining area for future collisions.
+            "apple"//Declared a tag for apple object.
+        ]);
+    };
+//End.
 
-    //Column position. Vertical.
-    for(let x = 0; x <= COLS; x++) {
-        //const x = startX + col*GRID_SIZE
-        drawLine({
-            p1: vec2(x * GRID_SIZE, 0),
-            p2: vec2(x * GRID_SIZE, ROWS * GRID_SIZE), 
-            width: 1,
-            color: rgb(0, 0, 0)
-        });
-    }
-    
-    //Row position. Horizont.
-    for(let y = 0; y <= ROWS; y++) {
-        //const y = startY + row*GRID_SIZE
-        drawLine({
-            p1: vec2(0, y * GRID_SIZE),
-            p2: vec2(COLS * GRID_SIZE, y * GRID_SIZE), 
-            width: 1,
-            color: rgb(0, 0, 0)
-        });
-    }
-}
+//Score att rotine and change player spped for each apple eated.
+    let score = add([//Created for show score text in screen.
+        k.text("Score: 0"),
+        k.pos(24,24),
+        { value:0}//Score value, this variable will be increased.
+    ]);
 
-//Movement mechanic. Adjust the default direction value for the key that was pressed
-onKeyDown("up", () => {
-    player.currentDir = "up"
-})
-onKeyDown("down", () => {
-    player.currentDir = "down"
-})
-onKeyDown("left", () => {
-    player.currentDir = "left"
-})
-onKeyDown("right", () => {
-    player.currentDir = "right"
-})
+    player.onCollide("apple", (apple) => {
+        k.destroy(apple);
+        spawnApple();
 
-drawLine({
-    p1: vec2(0),
-    p2: mousePos(),
-    width: 4,
-    color: rgb(0, 0, 255),
-})
+        score.value+=1;
+        score.text="Score: " + score.value;
 
-//Update player movement, frame a frame.
-onUpdate(() => {
-    if (!player.currentDir) return
-    switch (player.currentDir) {
-        case "up":
-            player.pos.y -= player.speed * dt()
-            break
-        case "down":
-            player.pos.y += player.speed * dt()
-            break
-        case "left":
-            player.pos.x -= player.speed * dt()
-            break
-        case "right":
-            player.pos.x += player.speed * dt()
-            break
-    }
-})
-// Draw the grid
-drawGrid();
+        player.speed+=10;
+
+        //Verify if snakebody have any segment. If yes(snakeBody.length > 0), pick the last body segment(snakeBody[snakeBody.length - 1]). If no, use the snake head with reference. On resume, if snake have body, use the last segment with parameter, if no have body, use the snake head with parameter.
+        const lastSegment = snakeBody.length > 0 ? snakeBody[snakeBody.length - 1]: player;
+        const newSegment = add([
+            k.sprite("player"),
+            k.pos(lastSegment.pos),
+            k.area(),
+            "snake-body"
+        ]);
+        //Increment a new segment on snake body array.
+        snakeBody.push(newSegment);
+    });
+//End.
+
+//Movement mechanic. Adjust the default direction value for the key that was pressed.
+    onKeyDown("up",() => {
+        player.currentDir="up"
+    });
+    onKeyDown("down",() => {
+        player.currentDir="down"
+    });
+    onKeyDown("left",() => {
+        player.currentDir="left"
+    });
+    onKeyDown("right",() => {
+        player.currentDir="right"
+    });
+//End.
+
+//Update player movement, frame by frame.
+    onUpdate(() => {
+        //Temporary array for save actual snake position.
+        const previousPositions = [];
+        //Saving actual snake head position. player.pos.clone() make's a copy of snake head for avoid change original value.
+        previousPositions.push(player.pos.clone());
+
+        //Saving each snake body piece.
+        for(let i = 0; i < snakeBody.length; i++){
+            //Clone position of item in array for previousPositions.
+            previousPositions.push(snakeBody[i].pos.clone());
+        };
+
+        switch (player.currentDir) {
+            case "up":
+                player.pos.y -= player.speed * dt() //dt() is the time since the last frame, used so that the speed is consistent even with different FPS rates.
+                break;
+            case "down":
+                player.pos.y += player.speed * dt()
+                break;
+            case "left":
+                player.pos.x -= player.speed * dt()
+                break;
+            case "right":
+                player.pos.x += player.speed * dt()
+                break;
+        };
+        
+        //It causes each segment of the body to move to the previous position of the segment in front. Example: segment 1 go to snake head position previous, segment 2 go to where the segment 1 was.
+        for(let i = 0; i < snakeBody.length; i++){
+            snakeBody[i].pos = previousPositions[i];
+        };
+    });
+//End.
+
+//Call the function for spawn first apple.
+    spawnApple();
+//End.
+
+});
+
+//Run game again.
+go("game");
