93c52c86730c466bbe5d4b6667772d83bca75118
Víctor <142942997+viictor0906@users.noreply.github.com>
2025-04-24 11:44:07 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,14 +7,15 @@
         height:600, //Y
         stretch:false, //Define if screen game gonna stretch with screen size values. 
         letterbox:true, //Create edges on screen size rounded, put the corners on the middle.
-        background:[0,0,0]
+        background:[0,0,0] //Black.
     });
 //End.
 
+
 //Loading sprites.
-    const playerSprite=k.loadSprite("player","sprites/Chrome.png");
+    const playerSprite=k.loadSprite("player","sprites/chromeSprite.png");
     const candySprite=k.loadSprite("candy","sprites/Ram.png");
-    const gameBackgroundSprite=k.loadSprite("gameBackground","sprites/surface.png");
+    const gameBackgroundSprite=k.loadSprite("gameBackground","sprites/waterfall2.jpg");
     const snakeTitleSprite=k.loadSprite("snakeTitleSprite","sprites/snakeTitleSprite.png");
     const startTextSprite=k.loadSprite("startTextSprite","sprites/startTextSprite.png");
     const gameOverTextSprite=k.loadSprite("gameOverTextSprite","sprites/gameOverTextSprite.png");
@@ -23,13 +24,13 @@
     const motivationTextSprite=k.loadSprite("motivationTextSprite","sprites/motivationTextSprite.png");
 //End.
 
+
 //Defining game scene and start mechanic game programming.
     k.scene("game",()=>{
         //Setup background color.
             k.add([
                 k.sprite(gameBackgroundSprite),
-                k.pos(0,0),
-                k.scale(1.4),
+                k.scale(1),
                 "background"
             ]);
 
@@ -39,7 +40,6 @@
                 k.scale(0.1),
                 k.pos(center()),
                 k.area(), //Setup object collison.
-                k.body(), //Setup object gravity.
                 {speed:200,currentDir:"up"},
                 "snake-head" //Tag to call the object more easily.
             ]);
@@ -60,6 +60,7 @@
             };
         //End.
 
+
         //Score att rotine and change player spped for each apple eated.
             k.add([
                 k.sprite(scoreTextSprite),
@@ -71,26 +72,27 @@
                 k.text(""),
                 k.scale(1),
                 k.pos(180,22),
-                {value:0} //Score value, this variable will be increased.
+                {value:0} //Score value, this value will be increased.
             ]);
+
 
             //Verify collision between player and candy.
             player.onCollide("candy",(candy)=>{
                 k.destroy(candy); //Destroy object.
                 spawnApple(); //Call the function will be spawn candy in random positions after collide.
-
-                //k.addKaboom(player.pos);
-                    //k.shake();
+ 
 
                 score.value+=1; //Increase new value on score value variable.
                 score.text=""+score.value; //Update text with new value.
 
+
                 player.speed+=5; //Increases player speed value.
+
 
                 //Verify if snakebody have any segment. If yes(snakeBody.length > 0), pick the last body segment(snakeBody[snakeBody.length - 1]). If no, use the snake head with reference. On resume, if snake have body, use the last segment with parameter, if no have body, use the snake head with parameter.
                 const lastSegment=snakeBody.length>0 ? snakeBody[snakeBody.length-1]:player;
                 const newSegment=add([
-                    k.sprite("player"),
+                    k.sprite(playerSprite),
                     k.scale(0.1),
                     k.pos(lastSegment.pos),
                     k.area(),
@@ -101,20 +103,22 @@
             });
         //End.
 
+
         //Movement mechanic. Adjust the default direction value for the key that was pressed.
-            onKeyDown("up",()=>{
+            k.onKeyDown("up",()=>{
                 player.currentDir="up" //The lib reference directions based on keyboard dpad.
             });
-            onKeyDown("down",()=>{
+            k.onKeyDown("down",()=>{
                 player.currentDir="down"
             });
-            onKeyDown("left",()=>{
+            k.onKeyDown("left",()=>{
                 player.currentDir="left"
             });
-            onKeyDown("right",()=>{
+            k.onKeyDown("right",()=>{
                 player.currentDir="right"
             });
         //End.
+
 
         //Function to check distance between player sprite and snake body.
             function closePositions(position1,position2){ //Position parameters that will checked.
@@ -123,14 +127,17 @@
             };
         //End.   
 
+
         //Update game for each frame and action in game. I'm unique all mechanics what need update by frame in just one 'onUpdate' for best organize and less redundacy, because all this things use onUpdate, so, use just one work's normarly.
             k.onUpdate(()=>{
                 //Update player movement, frame by frame.
                     //Temporary array for save actual snake position.
                         const previousPosition=[];
 
+
                     //Saving actual snake head position. player.pos.clone() make's a copy of snake head for avoid change original value.
                         previousPosition.push(player.pos.clone());
+
 
                     //Saving each snake body piece.
                         for(let i=0;i<snakeBody.length;i++){
@@ -138,6 +145,7 @@
                             previousPosition.push(snakeBody[i].pos.clone());
                         };
                     //End.
+
 
                     //Change player movement direction.
                         switch(player.currentDir){
@@ -156,6 +164,7 @@
                         };
                     //End.
                     
+
                     //It causes each segment of the body to move to the previous position of the segment in front. Example: segment 1 go to snake head position previous, segment 2 go to where the segment 1 was.
                         for(let i=0;i<snakeBody.length;i++){
                             snakeBody[i].pos=previousPosition[i];
@@ -163,28 +172,30 @@
                     //End.
                 //End.
 
+
                 //Makes collision system to check if the snake touch screen edge, if yes, turn back.
                     //0 is the inicial position of screen limit, it's work to height too.
                     switch(true){
                         case(player.pos.x<0):
                             player.currentDir="right";
-                        break;
+                            break;
 
                         //Final limit of screen, it's work to height too.
                         case(player.pos.x>755):
                             player.currentDir="left";
-                        break;
+                            break;
 
                         case(player.pos.y<0):
                             player.currentDir="down";
-                        break;
+                            break;
 
                         case(player.pos.y>550):
                             player.currentDir="up";
-                        break;
+                            break;
                     };
                 //End.
-                
+             
+
                 //Rotine to check collision between player and snake body array.
                     for(let i=20;i<snakeBody.length;i++){ //i=30 for check after snake body get big, because if i=0, will always check, as they are very close.
                         if(closePositions(player.pos,snakeBody[i].pos)){
@@ -195,6 +206,7 @@
             });
         //End.
     
+
         //Call the function for spawn first apple, after first spawn, the others block code will work.
             spawnApple();
 });
@@ -211,13 +223,15 @@
                 k.add([
                     k.sprite(startTextSprite),  
                     k.scale(0.5),
-                    k.color(rgb(255,0,0)),
+                    k.color(rgb(255,0,0)), //Red.
                     k.pos(-15,100),
                     retryGame(),
                     "start-text"
                 ]);
             });
         //End.
+    
+
 
         //Game over scene, used when player collide with snake body after 30th segment.
             k.scene("game-over",()=>{
@@ -231,7 +245,7 @@
                 k.add([
                     k.sprite(retryTextSprite),
                     k.scale(0.5),
-                    k.color(rgb(255,0,0)),
+                    k.color(rgb(255,0,0)), //Red.
                     k.pos(20,100),
                     retryGame(), //Call retryGame function.
                     "retry-text-sprite"
@@ -252,6 +266,7 @@
                 })
             };
         //End.
-    
+
+
     //Start on main menu scene.
         go("main-menu");
